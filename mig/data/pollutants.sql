cb8ab23a84c7ebb8c84f4d03986291e130b2d5fb
Raphaël Dhôte <raphael.dhote@gmail.com>
2021-02-07 13:13:25 +0100

    add missing pollutant units

--- mig/data/pollutants.sql
+++ mig/data/pollutants.sql
@@ -1,5 +1,5 @@
 INSERT INTO pollutants (full_name, short_name, description, waqi_name, is_pollutant, unit)
-VALUES ('Indice de qualité de l''air', 'AQI', '', 'aqi', TRUE),
+VALUES ('Indice de qualité de l''air', 'AQI', '', 'aqi', TRUE, ''),
        ('Monoxyde de carbone', 'CO', '', 'co', TRUE, 'ppm'),
        ('Humidité', 'H', '', 'h', FALSE, '%'),
        ('Dioxyde d''azote', 'NO2', '', 'no2', TRUE, 'µg/m³'),
@@ -9,6 +9,6 @@ VALUES ('Indice de qualité de l''air', 'AQI', '', 'aqi', TRUE),
        ('Particules fines PM2.5', 'PM25', '', 'pm25', TRUE, 'µg/m³'),
        ('Dioxyde de soufre', 'SO2', '', 'so2', TRUE, 'µg/m³'),
        ('Température', 'T', '', 't', FALSE, '°C'),
-       ('Inconnu', 'DEW', '', 'dew', FALSE),
+       ('Inconnu', 'DEW', '', 'dew', FALSE, ''),
        ('Vent', 'W', '', 'w', FALSE, 'm/s'),
-       ('Inconnu', 'WG', '', 'wg', FALSE);
+       ('Inconnu', 'WG', '', 'wg', FALSE, '');
